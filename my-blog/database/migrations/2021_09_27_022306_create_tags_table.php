0cdd489b07f0648e6485fe5f51aaeb3f8b5204de
Nick Nguyen <55971813+ninhdangcap123@users.noreply.github.com>
2021-09-27 09:32:27 +0700

    Migrations

---
+++ my-blog/database/migrations/2021_09_27_022306_create_tags_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTagsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+        Schema::create('article_tag', function (Blueprint $table){
+            $table->integer('article_id');
+            $table->integer('tag_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('article_tag');
+    }
+}
